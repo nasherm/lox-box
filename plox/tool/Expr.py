f45a491a4dd7a335c9c49db7e47b011ed7b99f4e
nashpotato <mncubenashe@gmail.com>
2020-10-18 15:00:12 +0100

    A AST source builder

--- plox/tool/Expr.py
+++ plox/tool/Expr.py
@@ -1,18 +1,39 @@
 from typing import Any
+from plox.plox.Token import Token
 class Expr:
 	pass
+	def accept(self,visitor): pass
+class Visitor:
+	def visitBinaryExpr(self,Binaryexpr:Expr): pass
+	def visitGroupingExpr(self,Groupingexpr:Expr): pass
+	def visitLiteralExpr(self,Literalexpr:Expr): pass
+	def visitUnaryExpr(self,Unaryexpr:Expr): pass
+
 class Binary(Expr):
 	def __init__(self,left:Expr, operator:Token, right:Expr):
 		self.left = left
 		self. operator =  operator
 		self. right =  right
+	def accept(self, visitor:Visitor):
+		return visitor.visitBinaryExpr(self)
+
 class Grouping(Expr):
 	def __init__(self,expression:Expr):
 		self.expression = expression
+	def accept(self, visitor:Visitor):
+		return visitor.visitGroupingExpr(self)
+
 class Literal(Expr):
 	def __init__(self,value:Any):
 		self.value = value
+	def accept(self, visitor:Visitor):
+		return visitor.visitLiteralExpr(self)
+
 class Unary(Expr):
 	def __init__(self,operator:Token, right:Expr):
 		self.operator = operator
 		self. right =  right
+	def accept(self, visitor:Visitor):
+		return visitor.visitUnaryExpr(self)
+
+
--- plox/tool/GenerateAst.py
+++ plox/tool/GenerateAst.py
@@ -22,11 +22,15 @@ class GenerateAst:
         path = f'{self.outputDir}/{baseName}.py'
         fileWriter = open(path, 'w')
         fileWriter.write('from typing import Any\n')
+        fileWriter.write('from plox.plox.Token import Token\n')
         fileWriter.write(f'class {baseName}:\n\tpass\n')
+        fileWriter.write(f'\tdef accept(self,visitor): pass\n')
+        self.defineVisitor(fileWriter, baseName, types)
         for type in types:
             className, fields = [
                 x.strip() for x in type.split(';')]
             self.defineType(fileWriter, baseName, className, fields)
+        fileWriter.write('\n')
         fileWriter.close()
 
     def defineType(self, fileWriter, baseName, className, fields):
@@ -35,5 +39,15 @@ class GenerateAst:
         for field in fields.split(','):
             name = field.split(':')[0]
             fileWriter.write(f'\t\tself.{name} = {name}\n')
+        fileWriter.write(f'\tdef accept(self, visitor:Visitor):\n')
+        fileWriter.write(f'\t\treturn visitor.visit{className}{baseName}(self)\n')
+        fileWriter.write('\n')
 
-# GenerateAst().run()
+    def defineVisitor(self, fileWriter, baseName, types):
+        fileWriter.write('class Visitor:\n')
+        for type in types:
+            typeName = type.split(';')[0].strip()
+            fileWriter.write(f'\tdef visit{typeName}{baseName}(self,{typeName}{baseName.lower()}:Expr): pass\n')
+        fileWriter.write('\n')
+
+GenerateAst().run()
