e441b4b9195e50675370eb0e92e6092d7e01b0e9
Nashe Mncube <mncubenashe@gmail.com>
2020-10-30 10:05:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="plox-YOUR-USERNAME-HERE", # Replace with your own username
+    name="plox-nasherm", # Replace with your own username
     version="0.0.1",
     author="Nashe Mncube",
     author_email="mncubenashe@gmailcpm",
