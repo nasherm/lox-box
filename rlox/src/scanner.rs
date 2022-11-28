e975ed10010deaf41c14973be140ef85727460ab
Nashe Mncube <mncubenashe@gmail.com>
2022-11-28 17:40:09 +0000

    Testing for scanner
    Implemented tests for scanner.

--- rlox/src/scanner.rs
+++ rlox/src/scanner.rs
@@ -21,11 +21,13 @@ pub enum TokenType {
     TOKEN_ERROR, TOKEN_EOF
 }
 
+#[derive(Debug)]
 pub struct Token {
     pub token_type: TokenType,
     pub line: i32,
     pub start: usize,
     pub length: usize,
+    pub option_string: Option<String>,
 }
 
 pub struct Scanner {
@@ -66,32 +68,42 @@ impl Scanner {
 
     pub fn token_chars(&self, length: usize) -> String {
         let mut s = String::new();
-        for i in self.current..self.current + length {
+        for i in self.current-length..self.current {
             s.push(self.source_code[i]);
         }
         return s
     }
 
     fn is_at_end(&self) -> bool {
+        if self.current >= self.source_code.len() {
+            return true
+        }
         self.source_code[self.current] == '\0'
     }
 
     fn error_token(&self, error_message:&str) -> Token {
-        print!("FAILURE: line={:?} error={:?}", self.line, error_message);
         Token {
             token_type: TokenType::TOKEN_ERROR,
             line: self.line,
             start: self.start,
             length: 0,
+            option_string: Some(String::from(error_message)),
         }
     }
 
     fn make_token(&self, token_type: TokenType) -> Token {
+        let option_string = match token_type {
+            TokenType::TOKEN_STRING |
+            TokenType::TOKEN_NUMBER |
+            TokenType::TOKEN_IDENTIFIER => Some(self.token_chars(self.current - self.start)),
+            _ => None,
+        };
         Token {
             token_type: token_type,
             line: self.line,
             start: self.start,
             length: self.current - self.start,
+            option_string: option_string,
         }
     }
 
@@ -119,6 +131,9 @@ impl Scanner {
     }
 
     fn peek(&self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
         self.source_code[self.current]
     }
 
@@ -149,6 +164,9 @@ impl Scanner {
         word:&str,
         expected_type: TokenType) -> bool
     {
+        if self.start + length >= self.source_code.len() {
+            return false
+        }
         let l = word.chars();
         let r = self.source_code[start..start + length].iter();
         std::iter::zip(l, r).fold(true, |acc, x| acc && match x {
@@ -156,22 +174,29 @@ impl Scanner {
         })
     }
 
-    fn identifier_type(&mut self, start: usize) -> TokenType {
+    fn identifier_type(&mut self, start: usize) -> Option<(TokenType, usize)> {
         for (keyword, token) in keyword_array.iter() {
-            if self.check_keyword(start, keyword.len(), keyword, *token) {
-                return *token;
+            if self.check_keyword(start, keyword.len() - 1, keyword, *token) {
+                return Some((*token, keyword.len()));
             }
         }
-        TokenType::TOKEN_IDENTIFIER
+        None
     }
 
     fn identifier(&mut self) -> Token {
         let start = self.current;
-        while self.is_alpha(self.peek()) || self.is_digit(self.peek()) {
-            self.advance();
-        }
-        let id_type = self.identifier_type(start);
-        return self.make_token(id_type);
+        let token = match self.identifier_type(start) {
+            Some((token, len)) => {
+                self.current += len;
+                token
+            },
+            _ => {
+                while self.is_alpha(self.peek()) { self.advance(); }
+                TokenType::TOKEN_IDENTIFIER
+            }
+        };
+
+        return self.make_token(token);
     }
 
     fn string(&mut self) -> Token {
@@ -266,4 +291,94 @@ impl Scanner {
 
         self.error_token("Unexpected character.")
    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::scanner::{Scanner, Token, TokenType};
+    fn drive_tokens(test_string:&str) -> Vec<Token> {
+        let input_string = String::from(test_string);
+        let mut scanner = Scanner::init(&input_string);
+        let mut scanned_tokens: Vec<Token> = Vec::new();
+        loop {
+            let token = scanner.scan_token();
+            if token.token_type == TokenType::TOKEN_EOF {
+                break
+            }
+            scanned_tokens.push(token);
+        }
+        scanned_tokens
+    }
+
+    #[test]
+    fn simple_tokens() {
+        let scanned_tokens = drive_tokens("(){};,.-+/*");
+        assert_eq!(scanned_tokens[0].token_type, TokenType::TOKEN_LEFT_PAREN);
+        assert_eq!(scanned_tokens[1].token_type, TokenType::TOKEN_RIGHT_PAREN);
+        assert_eq!(scanned_tokens[2].token_type, TokenType::TOKEN_LEFT_BRACE);
+        assert_eq!(scanned_tokens[3].token_type, TokenType::TOKEN_RIGHT_BRACE);
+        assert_eq!(scanned_tokens[4].token_type, TokenType::TOKEN_SEMICOLON);
+        assert_eq!(scanned_tokens[5].token_type, TokenType::TOKEN_COMMA);
+        assert_eq!(scanned_tokens[6].token_type, TokenType::TOKEN_DOT);
+        assert_eq!(scanned_tokens[7].token_type, TokenType::TOKEN_MINUS);
+        assert_eq!(scanned_tokens[8].token_type, TokenType::TOKEN_PLUS);
+        assert_eq!(scanned_tokens[9].token_type, TokenType::TOKEN_SLASH);
+        assert_eq!(scanned_tokens[10].token_type, TokenType::TOKEN_STAR);
+    }
+
+    #[test]
+    fn conditional_tokens() {
+        let scanned_tokens = drive_tokens("!!=><>=<====");
+        assert_eq!(scanned_tokens[0].token_type, TokenType::TOKEN_BANG);
+        assert_eq!(scanned_tokens[1].token_type, TokenType::TOKEN_BANG_EQUAL);
+        assert_eq!(scanned_tokens[2].token_type, TokenType::TOKEN_GREATER);
+        assert_eq!(scanned_tokens[3].token_type, TokenType::TOKEN_LESS);
+        assert_eq!(scanned_tokens[4].token_type, TokenType::TOKEN_GREATER_EQUAL);
+        assert_eq!(scanned_tokens[5].token_type, TokenType::TOKEN_LESS_EQUAL);
+        assert_eq!(scanned_tokens[6].token_type, TokenType::TOKEN_EQUAL_EQUAL);
+        assert_eq!(scanned_tokens[7].token_type, TokenType::TOKEN_EQUAL);
+    }
+
+    #[test]
+    fn string_tokens() {
+        let scanned_tokens = drive_tokens("\"Hello, world!\"");
+        assert_eq!(scanned_tokens[0].token_type, TokenType::TOKEN_STRING);
+        assert_eq!(scanned_tokens[0].option_string, Some(String::from("\"Hello, world!\"")));
+    }
+
+    #[test]
+    fn numeric_tokens() {
+        let scanned_tokens = drive_tokens("3.14159542");
+        assert_eq!(scanned_tokens[0].token_type, TokenType::TOKEN_NUMBER);
+        assert_eq!(scanned_tokens[0].option_string, Some(String::from("3.14159542")));
+    }
+
+    #[test]
+    fn identifier_tokens() {
+        let scanned_tokens = drive_tokens("my_random_func");
+        assert_eq!(scanned_tokens[0].token_type, TokenType::TOKEN_IDENTIFIER);
+        assert_eq!(scanned_tokens[0].option_string, Some(String::from("my_random_func")));
+    }
+
+    #[test]
+    fn keyword_tokens() {
+        let test_string ="andclasselseifnilorprintreturnsupervarwhilefalseforfunthistrue";
+        let scanned_tokens = drive_tokens(test_string);
+        assert_eq!(scanned_tokens[0].token_type, TokenType::TOKEN_AND);
+        assert_eq!(scanned_tokens[1].token_type, TokenType::TOKEN_CLASS);
+        assert_eq!(scanned_tokens[2].token_type, TokenType::TOKEN_ELSE);
+        assert_eq!(scanned_tokens[3].token_type, TokenType::TOKEN_IF);
+        assert_eq!(scanned_tokens[4].token_type, TokenType::TOKEN_NIL);
+        assert_eq!(scanned_tokens[5].token_type, TokenType::TOKEN_OR);
+        assert_eq!(scanned_tokens[6].token_type, TokenType::TOKEN_PRINT);
+        assert_eq!(scanned_tokens[7].token_type, TokenType::TOKEN_RETURN);
+        assert_eq!(scanned_tokens[8].token_type, TokenType::TOKEN_SUPER);
+        assert_eq!(scanned_tokens[9].token_type, TokenType::TOKEN_VAR);
+        assert_eq!(scanned_tokens[10].token_type, TokenType::TOKEN_WHILE);
+        assert_eq!(scanned_tokens[11].token_type, TokenType::TOKEN_FALSE);
+        assert_eq!(scanned_tokens[12].token_type, TokenType::TOKEN_FOR);
+        assert_eq!(scanned_tokens[13].token_type, TokenType::TOKEN_FUN);
+        assert_eq!(scanned_tokens[14].token_type, TokenType::TOKEN_THIS);
+        assert_eq!(scanned_tokens[15].token_type, TokenType::TOKEN_TRUE);
+    }
 }
